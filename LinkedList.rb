87ee5fc32dd544baf9bd321135bf9f9cd2fd9dbd
Jesus <jesuswrivas@gmail.com>
2023-02-25 22:33:48 -0500

    Size method completed
    Create size method on LinkedList class

--- LinkedList.rb
+++ LinkedList.rb
@@ -22,27 +22,48 @@ class LinkedList
 
     def append(value)
         current_value = @head
-        unless current_value.pointer == nil
-            puts current_value
-            current_value = current_value.pointer 
+
+        loop do
+            current_value = current_value.pointer
+            break if current_value.pointer == nil      
         end
+        
         current_value.pointer = value
         value.pointer = nil   
 
     end
 
 
+    def size
+
+        return 0 if @head == nil
+
+        current_value = @head
+        next_value = current_value.pointer
+        counter = 1
+
+        while next_value != nil
+            current_value = next_value
+            next_value = current_value.pointer
+            counter += 1
+                
+        end
+        return counter      
+    end
+    
+
+
 end
-
-
 
 
 nodeA = Node.new(4)
 nodeB = Node.new(10)
 nodeC = Node.new(33)
+nodeD = Node.new(44)
 
 linked_list = LinkedList.new
 
-linked_list.prepend(nodeA)
-linked_list.prepend(nodeB)
-linked_list.append(nodeC)
+linked_list.prepend(nodeA) 
+
+#linked_list.prepend(nodeB) 
+#linked_list.append(nodeC)
