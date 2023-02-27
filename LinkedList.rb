9cc3216d1ec106b36e06f0218d10a02d22e43423
Jesus <jesuswrivas@gmail.com>
2023-02-27 08:14:56 -0500

    Create pop method.
    Pop method uses previously defined tail and at index methods. Pop will
    return the value object

--- LinkedList.rb
+++ LinkedList.rb
@@ -2,11 +2,11 @@ require_relative 'Node.rb'
 
 class LinkedList
 
-    attr_accessor :head, :tail
+    attr_accessor :head
 
     def initialize
         @head = nil
-        @tail = nil
+       
     end
 
     def prepend(value)
@@ -21,16 +21,22 @@ class LinkedList
     end
 
     def append(value)
-        current_value = @head
 
-        loop do
-            current_value = current_value.pointer
-            break if current_value.pointer == nil      
+        if @head == nil
+            @head = value
+        else
+
+            current_value = @head
+
+            loop do
+                current_value = current_value.pointer
+                break if current_value.pointer == nil      
+            end
+            
+            current_value.pointer = value
+            value.pointer = nil
         end
-        
-        current_value.pointer = value
-        value.pointer = nil   
-
+           
     end
 
 
@@ -39,20 +45,63 @@ class LinkedList
         return 0 if @head == nil
 
         current_value = @head
-        next_value = current_value.pointer
         counter = 1
 
-        while next_value != nil
-            current_value = next_value
-            next_value = current_value.pointer
+        while current_value.pointer != nil
+            current_value = current_value.pointer
             counter += 1
                 
         end
         return counter      
     end
     
+    def head
+        @head
+    end
 
 
+    def tail
+        #return nil if @head == nil
+
+        current_value = @head
+       
+        while current_value.pointer != nil
+            current_value = current_value.pointer               
+        end
+        return current_value    
+    end
+
+
+    def at_index(index)
+        
+        current_value = @head
+        i = 0
+       
+        while i < index
+            current_value = current_value.pointer 
+            i += 1              
+        end
+        return current_value   
+
+    end
+
+
+    def pop
+        if self.size == 1
+            popped_value = self.tail
+            @head = nil
+            return popped_value
+        
+        else
+            popped_value = self.tail.value
+            self.at_index(self.size-2).pointer = nil
+            return popped_value
+            
+        end
+        
+     end
+     
+    
 end
 
 
@@ -65,5 +114,6 @@ linked_list = LinkedList.new
 
 linked_list.prepend(nodeA) 
 
-#linked_list.prepend(nodeB) 
-#linked_list.append(nodeC)
+linked_list.prepend(nodeB) 
+linked_list.append(nodeC)
+linked_list.pop
